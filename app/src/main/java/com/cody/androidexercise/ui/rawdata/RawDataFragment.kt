1548c6410daa2497d0cce6526657707c1a4f95e3
Kalvin dela Raga <kalvin_delaraga@ligph.com>
2020-12-21 10:56:34 +0800

    load file from raw folder

---
+++ app/src/main/java/com/cody/androidexercise/ui/rawdata/RawDataFragment.kt
@@ -0,0 +1,54 @@
+package com.cody.androidexercise.ui.rawdata
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.*
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.cody.androidexercise.databinding.FragmentRawDataBinding
+
+class RawDataFragment : Fragment() {
+
+    private lateinit var binding: FragmentRawDataBinding
+
+    private lateinit var viewModel: RawDataViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRawDataBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+
+        viewModel = ViewModelProvider(requireActivity()).get(RawDataViewModel::class.java)
+        viewModel.readResult.observe(viewLifecycleOwner) {
+            setupReadResult(it)
+        }
+
+        binding.viewModel = viewModel
+        binding.actionLoad.setOnClickListener {
+            viewModel.readFile(requireContext())
+        }
+
+        return binding.root
+    }
+
+    private fun setupReadResult(result: RawDataReadResult) {
+        val context = requireContext()
+        when (result) {
+            RawDataReadResult.Initial -> {
+                Toast.makeText(context, "Welcome", Toast.LENGTH_SHORT).show()
+            }
+            RawDataReadResult.Ongoing -> {
+                Toast.makeText(context, "Reading...", Toast.LENGTH_SHORT).show()
+            }
+            is RawDataReadResult.Success -> {
+                viewModel.content.value = result.content
+                Toast.makeText(context, "Success!", Toast.LENGTH_SHORT).show()
+            }
+            is RawDataReadResult.Failure -> {
+                Toast.makeText(context, result.error, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/cody/androidexercise/ui/rawdata/RawDataViewModel.kt
@@ -0,0 +1,39 @@
+package com.cody.androidexercise.ui.rawdata
+
+import android.content.Context
+import androidx.lifecycle.*
+import com.cody.androidexercise.R
+import java.io.*
+import java.lang.StringBuilder
+
+class RawDataViewModel: ViewModel() {
+    val content: MutableLiveData<String> = MutableLiveData("")
+
+    private val _readResult: MutableLiveData<RawDataReadResult> = MutableLiveData(RawDataReadResult.Initial)
+    val readResult: LiveData<RawDataReadResult> = _readResult
+
+    fun readFile(context: Context) {
+        _readResult.value = RawDataReadResult.Ongoing
+
+        var reader: BufferedReader? = null
+
+        try {
+            val inputStream = context.resources.openRawResource(R.raw.rawfiles)
+            reader = BufferedReader(InputStreamReader(inputStream))
+            val builder = StringBuilder()
+            var str: String?
+            do {
+                str = reader.readLine()
+                str?.let {
+                    builder.append(it + "\n")
+                }
+            } while (str != null)
+
+            _readResult.value = RawDataReadResult.Success(builder.toString())
+        } catch (e: IOException) {
+            _readResult.value = RawDataReadResult.Failure(e.message ?: "Unknown error")
+        } finally {
+            reader?.close()
+        }
+    }
+}
