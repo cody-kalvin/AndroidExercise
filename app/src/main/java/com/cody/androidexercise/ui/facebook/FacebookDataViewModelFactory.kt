49dcb3619697914f2c3196c8d305d0dd48061e8f
Kalvin dela Raga <kalvin_delaraga@ligph.com>
2020-12-21 12:25:29 +0800

    apply linter for better readability

--- app/src/main/java/com/cody/androidexercise/ui/facebook/FacebookDataViewModelFactory.kt
+++ app/src/main/java/com/cody/androidexercise/ui/facebook/FacebookDataViewModelFactory.kt
@@ -7,11 +7,14 @@ import com.cody.androidexercise.data.facebook.FacebookGraphRepository
 import com.facebook.AccessToken
 import com.google.gson.Gson
 
-class FacebookDataViewModelFactory(private val application: Application): ViewModelProvider.AndroidViewModelFactory(application) {
+class FacebookDataViewModelFactory(
+    private val application: Application
+) : ViewModelProvider.AndroidViewModelFactory(application) {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(FacebookDataViewModel::class.java)) {
-            val accessToken = Gson().fromJson(CredentialRepository.facebookToken, AccessToken::class.java)
+            val accessToken =
+                Gson().fromJson(CredentialRepository.facebookToken, AccessToken::class.java)
             val repository = FacebookGraphRepository(accessToken)
             return FacebookDataViewModel(application, repository) as T
         }
